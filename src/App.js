9743ae6effbeb036f7e074b532106d5bca3d1d56
satyam kakra <satyamkakra4u@gmail.com>
2025-02-14 12:42:21 +0530

    route

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import Banner from './components/Banner';
 import NavBar from './components/NavBar';
 import MovieList from './components/MovieList';
 import Favourites from './components/Favourites';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
 
       <NavBar />
 
@@ -18,7 +18,7 @@ function App() {
         <Route path='/favourites' element={<Favourites />} />
       </Routes>
 
-    </Router>
+    </BrowserRouter>
 
   );
 }
